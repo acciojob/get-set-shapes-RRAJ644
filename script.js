5997407c7c6a4aee9a208a60117e058c7d5353ce
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-08 15:19:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;   // Use _ to signify internal properties
+    this._height = height; // Same here
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // Pass the side value as both width and height
+  }
+
+  getPerimeter() {
+    return 4 * this.width; // Use `this.width` or `this.height` as both are the same in a square
+  }
+}
+
+// Example usage:
+const square = new Square(5);
+console.log(square.getArea()); // Output: 25
+console.log(square.getPerimeter()); // Output: 20
+
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
